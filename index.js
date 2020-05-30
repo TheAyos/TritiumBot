6c02ef8d35917244ae1df531ca086a157464dca6
TheAyos <34219939+TheAyos@users.noreply.github.com>
2020-05-30 14:52:31 +0100

    leaving development

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-module.exports = bot;
+// This will check if the node version you are running is the required
+// Node version, if it isn't it will throw the following error to inform
+// you.
+if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
 
-const YoutubeStream = require('ytdl-core');
+// Load the discord.js library and other thingies
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
 
-const Invite = require('./commands/invite');
-const Google = require('./commands/google');
-const Ping = require('./commands/ping');
-const Help = require('./commands/help');
-const Info = require('./commands/info');
-const GuildInfo = require('./commands/guildinfo');
-const Profile = require('./commands/profile');
-const Play = require('./commands/play');
-const Skip = require('./commands/skip');
-const Stop = require('./commands/stop');
-const Trash = require('./commands/trash');
-const Coin = require('./commands/coin');
+// Declaring client
+const client = new Discord.Client();
+// Load config file that contains some useful values
+client.config = require("./config.json");
+client.logger = require("./modules/Logger.js");
 
-bot.on('message', function (msg) {
+client.commands = new Enmap();
+client.aliases = new Enmap();
 
-    var prefix = ("&");
-    var servers = {};
-    module.exports = servers;
-    // var args = msg.content.substring(prefix.length).split(" ");
-    let messageArray = msg.content.split(" ");
-    let args = messageArray.slice(1);
-    module.exports = args;
-
-    if (msg.author.bot || msg.channel.type !== 'text') return; // Do not respond to messages from bots or messages that are not from guilds.
-    if (msg.channel.type === "dm") return; //Do not respond to private messages
-
-    let commandUsed =
-        Coin.parse(msg) ||
-        Invite.parse(msg) ||
-        Trash.parse(msg) ||
-        Ping.parse(msg) ||
-        Help.parse(msg) ||
-        Info.parse(msg) ||
-        GuildInfo.parse(msg) ||
-        Profile.parse(msg) ||
-        Play.parse(msg) ||
-        Skip.parse(msg) ||
-        Stop.parse(msg) ||
-        Google.parse(msg)
+// Saves settings to disk
+client.settings = new Enmap({
+  name: "settings"
 });
 
-bot.on('ready', function () {
-    console.log(`Tritium connecté avec succés sous le nom de ${bot.user.username} !`);
-    console.log(`Serving ${bot.guilds.size} guilds and ${bot.users.size} users.`);
-    bot.user.setGame('[&help] Tritium 2.0 Patch').catch(console.error)
-});
+let servers = JSON.parse(fs.readFileSync("./servers.json", "utf8"));
+//var servers = {};
+client.servers = servers;
+//// TODO: change servers var to a file
 
-//bot.login('NDI2NDQzNzAxOTUxMTM1NzQ0.DZhzVg.a4A4XXWR1o3DtTyWSNw9UgrzKxU');
-bot.login(process.env.BOT_TOKEN);
 
-bot.on('error', (e) => console.error(e));
-bot.on('warn', (e) => console.warn(e));
+// This is a fancy function... i think
 
-bot.on('guildMemberAdd', function (member) {
-    member.createDM().then(function (channel) {
-        return channel.send('Bienvenue sur le serveur ' + member.displayName + ', moi, je suis Tritium, un bot Discord développé par **TheAyos** et je suis *enchanté* de te rencontrer!')
-    }).catch(console.error)
-});
+const initClient = async () => {
+
+  // Load events
+  const eventFiles = await fs.readdirSync("./events/");
+  client.logger.log(`Loading a total of ${eventFiles.length} events.`);
+  eventFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    client.logger.log(`Loading Event: ${eventName}`);
+    const event = require(`./events/${file}`);
+    // This line is awesome... they say
+    client.on(eventName, event.bind(null, client));
+  });
+
+  const commandFiles = await fs.readdirSync("./commands/");
+  client.logger.log(`Loading a total of ${commandFiles.length} commands.`);
+  commandFiles.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let commandName = file.split(".")[0];
+    try {
+      client.logger.log(`Attempting to load command ${commandName}`);
+      let props = require(`./commands/${commandName}`);
+      client.commands.set(commandName, props);
+      // props.conf.aliases.forEach(alias => {
+      //   client.aliases.set(alias, props.help.name);
+      // });
+    } catch (e) {
+      client.logger.log(`Unable to load command ${commandName}: ${e}`);
+    }
+  });
+};
+
+initClient();
+
+client.login(client.config.token);
+//bot.login(process.env.BOT_TOKEN);
